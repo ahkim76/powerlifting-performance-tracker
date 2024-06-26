873d738cbe6698e584858ade245ac389b25f57bd
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-06-26 19:49:21 -0400

    Update ProgressTracker.java

--- ProgressTracker.java
+++ ProgressTracker.java
@@ -1,21 +1,32 @@
-package Project;
-//import org.jfree.chart.ChartFactory;
+package com.alexkim.powerliftingperformancetrackerv2;
+
 import java.util.LinkedList;
 
 public class ProgressTracker {
-  //  private Workout lifts;
+    //  private Workout lifts;
     private LinkedList<Workout> workouts;
-    public ProgressTracker() {
+    private WilksScoreCalculator calculator;
+    private User user;
+    public ProgressTracker(User user) {
         workouts = new LinkedList<>();
+        calculator = new WilksScoreCalculator();
+        this.user = user;
     }
 
     public void addWorkouts(Workout workout) {
         workouts.add(workout);
     }
 
+
     public void viewProgress() {
         for (Workout workout : workouts) {
-           workout.displayDetails();
+            workout.displayDetails();
         }
     }
+    public void getWilksScore() {
+        double score = calculator.calculateWilksScore(user.getBodyweight(), user.getSquatPR()+user.getBenchPR()+user.getDeadliftPR(), user.getGender());
+        System.out.println(score);
+    }
+
+
 }
