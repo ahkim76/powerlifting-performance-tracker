50413112937191c4fbb758892a5177cc8cc52b67
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-06-28 17:31:46 -0400

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -1,14 +1,38 @@
 package com.alexkim.powerliftingperformancetrackerv2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import java.io.IOException;
+import java.util.Objects;
+import javafx.scene.image.Image;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+
+    public void switchToLoginPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Parent root = fxmlLoader.load();
+        scene = new Scene(root);
+
+        // Adding CSS to the scene
+        String css = Objects.requireNonNull(this.getClass().getResource("style.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
     }
 }
