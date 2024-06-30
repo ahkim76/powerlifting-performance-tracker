e2541509475792d8be05c1246c52a17af0cacb25
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-06-30 17:06:23 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
@@ -14,17 +16,26 @@ public class Main extends Application {
 
         // JAVAFX STUFF
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        String css = this.getClass().getResource("style.css").toExternalForm();
+        Scene scene = new Scene(fxmlLoader.load(), 711, 585);
+
+        //URL cssUrl = this.getClass().getResource("style.css");
+        //System.out.println("CSS URL: " + (cssUrl != null ? cssUrl.toExternalForm() : "null"));
+        //String css = Objects.requireNonNull(cssUrl).toExternalForm();
+        String css = Objects.requireNonNull(this.getClass().getResource("style.css")).toExternalForm();
+
+        //String css = this.getClass().getResource("style.css").toExternalForm();
         scene.getStylesheets().add(css);
+
         stage.setTitle("Powerlifting Performance Tracker");
+        stage.setResizable(false);
+
         stage.setScene(scene);
         stage.show();
 
 
         // Start any additional components after the UI is visible
         PowerliftingPerformanceTracker pl = new PowerliftingPerformanceTracker();
-        pl.run(); // Consider using Platform.runLater if this needs to interact with the UI
+        //pl.run(); // Consider using Platform.runLater if this needs to interact with the UI
         //Platform.runLater(() -> {
           //  PowerliftingPerformanceTracker pl = new PowerliftingPerformanceTracker();
             //pl.run();
