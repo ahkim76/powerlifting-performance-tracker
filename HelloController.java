c2dd266ed4f0d07e90148d2a623e677ff2e9abb5
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-06-30 23:43:28 -0400

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -8,13 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
-import javafx.scene.image.ImageView;
+
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
-import javafx.scene.image.Image;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
@@ -25,6 +25,27 @@ public class HelloController {
     private Pane focusPane;
 
     @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField squatPRField;
+
+    @FXML
+    private TextField benchPRField;
+
+    @FXML
+    private TextField deadliftPRField;
+
+    @FXML
+    private TextField bodyweightField;
+
+    @FXML
     private CheckBox femaleCheckBox;
 
     @FXML
@@ -33,6 +54,16 @@ public class HelloController {
     @FXML
     private AnchorPane registerPane;
 
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Alert errorAlert;
+
+    @FXML
+    private ChoiceBox<String> genderBox;
+
+    private String[] genders = {"Male", "Female"};
     /*
     @FXML
     private RadioButton maleRadioBtn;
@@ -44,13 +75,16 @@ public class HelloController {
 
     @FXML
     public void initialize() {
+
+        genderBox.getItems().add("hi");
+        genderBox.getItems().addAll(genders);
 /*
         genderToggleGroup = new ToggleGroup();
         maleRadioBtn = new RadioButton();
         femaleRadioBtn = new RadioButton();
         maleRadioBtn.setToggleGroup(genderToggleGroup);
         femaleRadioBtn.setToggleGroup(genderToggleGroup);
-        genderToggleGroup.selectToggle(maleRadioBtn);*/
+        genderToggleGroup.selectToggle(maleRadioBtn);
 
 
 
@@ -70,11 +104,11 @@ public class HelloController {
             } else {
                 femaleCheckBox.setSelected(true); // Ensure at least one is selected
             }
-        });
+        });*/
     }
 
 
-    public void switchToLoginPage(ActionEvent event) throws IOException {
+    public void switchToRegisterPage(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Register.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         Parent root = fxmlLoader.load();
@@ -86,5 +120,66 @@ public class HelloController {
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void switchToLoginPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Parent root = fxmlLoader.load();
+        scene = new Scene(root, 711, 585);
+        String css = Objects.requireNonNull(this.getClass().getResource("style.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void createAccount(ActionEvent event) throws IOException {
+
+
+        // Validate inputs
+        String name = firstNameField.getText();
+        String userID = usernameField.getText();
+        String password = passwordField.getText();
+        String squatPRStr = squatPRField.getText();
+        String benchPRStr = benchPRField.getText();
+        String deadliftPRStr = deadliftPRField.getText();
+        String bodyweightStr = bodyweightField.getText();
+
+        checkEmptyField(name);
+        checkEmptyField(userID);
+        checkEmptyField(password);
+        checkEmptyField(squatPRStr);
+        checkEmptyField(benchPRStr);
+        checkEmptyField(deadliftPRStr);
+        checkEmptyField(bodyweightStr);
+        boolean isMale = true;
+
+        try {
+            double squatPR = Double.parseDouble(squatPRStr);
+            double benchPR = Double.parseDouble(benchPRStr);
+            double deadliftPR = Double.parseDouble(deadliftPRStr);
+            double[] sbd = {squatPR, benchPR, deadliftPR};
+            double bodyweight = Double.parseDouble(bodyweightStr);
+            User newUser = new User(name, userID, sbd, bodyweight, isMale);
+            System.out.println(newUser);
+            System.out.println("~~~");
+        } catch(NumberFormatException e) {
+            errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Input is invalid");
+            errorAlert.setContentText("Name must be a valid String. PRs and bodyweight must be valid numbers.");
+            errorAlert.showAndWait();
+        }
+
+    }
+
+    private void checkEmptyField(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Input is invalid");
+            errorAlert.setContentText("You have either not entered all the fields.");
+            errorAlert.showAndWait();
+            return;
+        }
     }
 }
