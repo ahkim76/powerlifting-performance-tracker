6e46d4a3274ea9ae2f35878671aebee086849f6e
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-07-03 23:56:07 -0400

    Add files via upload

---
+++ MainMenuController.java
@@ -0,0 +1,34 @@
+package com.alexkim.powerliftingperformancetrackerv2;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+public class MainMenuController {
+    @FXML private Label squatLabel, benchLabel, deadliftLabel, bodyweightLabel;
+    private HelloController helloController;
+    public void setUserInformation(User user) {
+        squatLabel.setText("Squat PR: " + user.getSquatPR()+"lbs");
+        benchLabel.setText("Bench PR: " + user.getBenchPR()+"lbs");
+        deadliftLabel.setText("Deadlift PR: " + user.getDeadliftPR()+"lbs");
+        bodyweightLabel.setText("Bodyweight: " + user.getBodyweight()+"lbs");
+
+    }
+
+    public void setHelloController(HelloController helloController) {
+        this.helloController = helloController;
+    }
+
+    @FXML public void switchToStarterScreen(ActionEvent event) throws IOException {
+        //switchTo(event, "hello-view.fxml");
+        if (helloController != null) helloController.switchToStarterScreen(event);
+    }
+}
