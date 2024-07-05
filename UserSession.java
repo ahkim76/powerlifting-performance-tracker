e7cd6b3055bd09af7cd9dea99ebd72c1784f9b27
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-07-04 20:24:29 -0400

    Add files via upload

---
+++ UserSession.java
@@ -0,0 +1,20 @@
+package com.alexkim.powerliftingperformancetrackerv2;
+
+public class UserSession {
+    private static UserSession instance;
+    private User currentUser;
+    private UserSession() {}
+
+    public static synchronized UserSession getInstance() {
+        if (instance == null) instance = new UserSession();
+        return instance;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+}
