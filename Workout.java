9287a65c6115ac2a8d5960c38559c727bc169c02
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-07-04 20:45:18 -0400

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -1,30 +1,32 @@
-package Project;
+package com.alexkim.powerliftingperformancetrackerv2;
+
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.LinkedList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.time.LocalDate;
 
 public class Workout {
-    private Date date;
-    private ArrayList<Exercise> session;
+    private LocalDate date;
+    private LinkedList<Exercise> session;
     private Workout next;
     private User user;
 
-    public Workout(Date date, User user) {
+    public Workout(LocalDate date, User user) {
         this.date = date;
-        this.session = new ArrayList<>();
+        this.session = new LinkedList<>();
         this.next = null;
         this.user = user;
     }
 
-    public Date getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
-    public ArrayList<Exercise> getExercise() {
+    public LinkedList<Exercise> getExercise() {
         return session;
     }
 
@@ -60,8 +62,8 @@ public class Workout {
 
         while(!finished) {
             try {
-                System.out.println("Do you want to enter an exercise? (y/n)");
-                String response = scan.next();
+                System.out.println("Do you want to enter or delete an exercise? (y/d/n)");
+                String response = scan.next().trim();
                 // Process the response
                 if (response.equalsIgnoreCase("y")) {
                     // Prompt the user for exercise details
@@ -78,48 +80,69 @@ public class Workout {
                     addExercise(exercise);
 
                     // if exercise was 1 rep and greater than PRs, then replace PR with the exercise
+                    updatePRs(title, reps, weight);
+                }
+                else if (response.equalsIgnoreCase("d")) {
+                    if (session.isEmpty()){
+                        System.out.println("No workouts to delete!");
 
+                    } else {
+                        System.out.println("Okay! Here are your recent workouts you can delete");
+                        for (int i=0; i<session.size(); i++) System.out.println((i+1)+". "+session.get(i));
 
-                    if((reps == 1)) {
-                        switch(title.toLowerCase()) {
-                            case "squat":
-                                if (weight > user.getSquatPR()) {
-                                    user.setSquatPR(weight);
-                                    System.out.println("Congratulations! You hit a new " + weight + " pounds squat PR!");
-                                    break;
-                                }
-                            case "bench":
-                                if (weight > user.getBenchPR()) {
-                                    user.setBenchPR(weight);
-                                    System.out.println("Congratulations! You hit a new " + weight + " pounds bench PR!");
-                                    break;
-                                }
-                            case "deadlift":
-                                if (weight > user.getDeadliftPR()) {
-                                    user.setDeadliftPR(weight);
-                                    System.out.println("Congratulations! You hit a new " + weight + " pounds deadlift PR!");
-                                    break;
-                                }
+                        System.out.println("Enter the number of the workout to delete: ");
+                        int choice = scan.nextInt();
+                        if(choice > 0 && choice >= session.size()) {
+                            session.remove(choice-1);
+                            System.out.println("Workout successfully removed");
                         }
                     }
 
 
-                } else if (response.equalsIgnoreCase("n")) {
+                }
+                else if (response.equalsIgnoreCase("n")) {
                     finished=true;
                 } else {
 
                     // Invalid response
                     System.out.println("Error detecting response, try again");
-                    enterExercise(scan); // Recursive call to try again
+                    //enterExercise(scan); // Recursive call to try again
                 }
+
+
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input type. Please try again");
-                scan.next();
+                scan.nextLine();
             }
+
 
         }
 
 
     }
-}
 
+    private void updatePRs(String title, int reps, double weight) {
+        if((reps == 1)) {
+            switch(title.toLowerCase()) {
+                case "squat":
+                    if (weight > user.getSquatPR()) {
+                        user.setSquatPR(weight);
+                        System.out.println("Congratulations! You hit a new " + weight + " pounds squat PR!");
+                        break;
+                    }
+                case "bench":
+                    if (weight > user.getBenchPR()) {
+                        user.setBenchPR(weight);
+                        System.out.println("Congratulations! You hit a new " + weight + " pounds bench PR!");
+                        break;
+                    }
+                case "deadlift":
+                    if (weight > user.getDeadliftPR()) {
+                        user.setDeadliftPR(weight);
+                        System.out.println("Congratulations! You hit a new " + weight + " pounds deadlift PR!");
+                        break;
+                    }
+            }
+        }
+    }
+}
