f3a3ec56de314520bd811e9ce0410b30c2a39b5d
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-07-10 22:52:16 -0400

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS users (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 username TEXT NOT NULL UNIQUE,
@@ -9,7 +11,5 @@ bodyweight REAL NOT NULL,
 gender INTEGER NOT NULL DEFAULT 1  -- 1 for true/male, 0 for false/female
 );
 
-INSERT INTO users (username, password) VALUES ('alex_kim', '1234567890');
-INSERT INTO users (username, password) VALUES ('monkeyBoy69', 'poopy123');
-
-
+INSERT OR IGNORE INTO users (username, password, squat, bench, deadlift, bodyweight, gender) VALUES ('alex_kim', '1234567890', 100.0, 200.0, 300.0, 159.0, 1);
+INSERT OR IGNORE INTO users (username, password, squat, bench, deadlift, bodyweight, gender) VALUES ('monkeyBoy69', 'poopy123', 1.0, 1.0, 1.0, 1.0, 0);
