82df96fcea93e18f48732dd0dac7ee80d2fc01fc
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2024-07-10 21:20:22 -0400

    Update users.sql

--- users.sql
+++ users.sql
@@ -11,3 +11,46 @@ gender INTEGER NOT NULL DEFAULT 1  -- 1 for true/male, 0 for false/female
 
 INSERT INTO users (username, password) VALUES ('alex_kim', '1234567890');
 INSERT INTO users (username, password) VALUES ('monkeyBoy69', 'poopy123');
+
+
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL,
+squat REAL NOT NULL,
+bench REAL NOT NULL,
+deadlift REAL NOT NULL,
+bodyweight REAL NOT NULL,
+gender INTEGER NOT NULL DEFAULT 1  -- 1 for true/male, 0 for false/female
+);
+
+INSERT INTO users (username, password) VALUES ('alex_kim', '1234567890');
+INSERT INTO users (username, password) VALUES ('monkeyBoy69', 'poopy123');
+
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL,
+squat REAL NOT NULL,
+bench REAL NOT NULL,
+deadlift REAL NOT NULL,
+bodyweight REAL NOT NULL,
+gender INTEGER NOT NULL DEFAULT 1  -- 1 for true/male, 0 for false/female
+);
+
+INSERT INTO users (username, password) VALUES ('alex_kim', '1234567890');
+INSERT INTO users (username, password) VALUES ('monkeyBoy69', 'poopy123');
+
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL,
+squat REAL NOT NULL,
+bench REAL NOT NULL,
+deadlift REAL NOT NULL,
+bodyweight REAL NOT NULL,
+gender INTEGER NOT NULL DEFAULT 1  -- 1 for true/male, 0 for false/female
+);
+
+INSERT INTO users (username, password) VALUES ('alex_kim', '1234567890');
+INSERT INTO users (username, password) VALUES ('monkeyBoy69', 'poopy123');
